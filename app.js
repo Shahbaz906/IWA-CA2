c5f29d9b5b8a593481e26b6e14f3207e5d40e170
Shahbaz906 <shahbazmalikji@yahoo.com>
2021-04-29 13:18:10 +0000

    db. connect

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
+const mongoose = require ('mongoose');
+require('dotenv/config');
+
+
+
+
+// import
+
 
 
 //ROUTES
-app.get('/',(req, res)=> {
-    res.send('we are at home');
+app.get('/menu',(req, res)=> {
+    res.send('we are in Menu');
 });
 
 
-app.get('/posts',(req, res)=> {
-    res.send('we are on post');
-});
+// mongodb connection
+mongoose.connect( process.env.DB_CONNECTION, { useUnifiedTopology: true },
+() => console.log('connected to database'));
 
 
 
