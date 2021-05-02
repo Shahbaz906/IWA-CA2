830f395f42d0f0250d4517d79d252ed8c796307e
Shahbaz906 <shahbazmalikji@yahoo.com>
2021-05-02 15:43:53 +0000

    connect to app

--- app.js
+++ app.js
@@ -3,25 +3,35 @@ const mongoose = require ('mongoose');
 const bodyParser = require('body-parser');
 axios = require('axios');
 require('dotenv/config');
-
-var port = process.env.PORT || 8000;
 const app = express();
 
+
+var port = process.env.PORT || 8000;
+
+
 app.use(bodyParser.json())
+
+
+
+
 
 // import
 const playersRoute = require('./routes/players');
 
-app.set('view engine', 'ejs')
-//middleware
-app.use('/players', playersRoute);
+
+
+
+
 
 //ROUTES
 const playersRouter = require('./routes/players')
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false}))
 app.use(express.static('views/players'))
 
 
-app.get('/',(req, res)=> {
+app.get('/', (req, res)=> {
     const players = [{
         title: 'Cricket Players List 2021',
         name:'anyone',
@@ -29,15 +39,19 @@ app.get('/',(req, res)=> {
         country: 'any'
 
     }]
-    res.redirect('players/index', {players: players});
-});
+    res.render('players/index', {players: players});
+})
 
 
 
-// mongodb connection
-//mongoose.connect( process.env.DB_CONNECTION, { useUnifiedTopology: true },
-//() => console.log('connected to database'));
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, }) 
 
+const db = mongoose.connection
 
+db.once('open', () => console.log('Connected to DB'))
+db.on('error', (error) => console.error(error))
+ 
 
+//middleware
+app.use('/players', playersRoute);
 app.listen(8000);
